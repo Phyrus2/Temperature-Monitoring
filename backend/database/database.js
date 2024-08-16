323d782580091b81d44d0fcf6742507bbcb8b7bf
Yuda <madeyudaadiwinata@gmail.com>
2024-08-16 09:42:15 +0800

    Update database.js

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,17 +1,17 @@
-// database.js
+require('dotenv').config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_rpi",
-  connectTimeout: 60000, // Increase database connection timeout
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  connectTimeout:60000, // Ensure the timeout is a number
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Error connecting to database:", err);
+    console.error("Error connecting to database:", err.message);
     return;
   }
   console.log("Database connected!");
