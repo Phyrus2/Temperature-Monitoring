d98c2b3626f993d7f00ece845741a2ed45354b0c
Yuda <madeyudaadiwinata@gmail.com>
2024-08-19 09:40:50 +0800

    upp

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,11 +1,12 @@
 // database.js
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_rpi",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
   connectTimeout: 60000, // Increase database connection timeout
 });
 
