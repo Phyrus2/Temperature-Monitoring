3145a2a9e426b676959f33dabbd9614795ad5b01
Yuda <madeyudaadiwinata@gmail.com>
2024-08-31 09:54:19 +0800

    up fix scheduler

--- backend/main.js
+++ backend/main.js
@@ -5,7 +5,8 @@ const email = require("../backend/controller/email");
 const data = require("../backend/controller/fetch-data");
 const schedule = require("node-schedule");
 require('./logger'); // This will override console.log and console.error
-const deleteLog = require("./deleteLog")
+const deleteLog = require("./deleteLog");
+const moment = require('moment-timezone');
 
 const app = express();
 const port = 3000;
@@ -15,17 +16,13 @@ app.use(express.json());
 
 setInterval(deleteLog, 86400000);
 
-const rule = new schedule.RecurrenceRule();
-rule.date = -1; // Last day of the month
-rule.hour = 23; // 11 PM
-rule.minute = 0; // 0 minutes
-rule.tz = "Asia/Makassar"; // WITA timezone
+const cronExpression = '48 9 L * *'; // 9:45 PM on the last day of the month (21 in UTC is 9 in WITA)
 
-schedule.scheduleJob(rule, async () => {
+schedule.scheduleJob(cronExpression, 'Asia/Makassar', async () => {
   try {
     await email.sendEmailForCurrentMonth();
     console.log(
-      `Email sent successfully of ${moment()
+      `Email sent successfully for ${moment()
         .tz("Asia/Makassar")
         .format("MMMM YYYY")}`
     );
@@ -33,6 +30,37 @@ schedule.scheduleJob(rule, async () => {
     console.error("Error sending email:", error);
   }
 });
+
+
+function scheduleLastDayOfMonthJob() {
+  const now = moment().tz('Asia/Makassar'); // Current time in WITA
+  const lastDayOfMonth = now.clone().endOf('month'); // Last day of the current month
+
+  // If today is the last day of the month, schedule the job
+  if (now.isSame(lastDayOfMonth, 'day')) {
+    const jobTime = lastDayOfMonth.clone().hour(23).minute(0); // Set time to 9:45 PM WITA
+
+    schedule.scheduleJob(jobTime.toDate(), async () => {
+      try {
+        await email.sendEmailForCurrentMonth();
+        console.log(
+          `Email sent successfully for ${moment()
+            .tz("Asia/Makassar")
+            .format("MMMM YYYY")}`
+        );
+      } catch (error) {
+        console.error("Error sending email:", error);
+      }
+    });
+
+    console.log(`Scheduled job for ${jobTime.format("MMMM Do YYYY, h:mm:ss a")}`);
+  } else {
+    console.log('Today is not the last day of the month; job not scheduled.');
+  }
+}
+
+scheduleLastDayOfMonthJob();
+
 
 app.get("/test-email", (req, res) => {
   email.sendEmailForCurrentMonth(res, req, 5, 2024)
